b07c40c07fffb11000da1f1d203970fcb7a5beb9
Luke Doman <ldoman@windhoverlabs.com>
2018-07-06 05:10:01 -0700

    Flow sensor done and cleaned up. Todo: test

--- fsw/src/sensors/flow.cpp
+++ fsw/src/sensors/flow.cpp
@@ -11,8 +11,6 @@
 
 #define FLOW_BETA_MAX         (700)
 #define FLOW_MIN_AGL         (0.5f) // TODO: Make param
-#define FLOW_MIN_QUALITY         (1) // TODO: Make param
-
 
 void PE::flowInit()
 {
@@ -26,7 +24,7 @@ void PE::flowInit()
 	}
 
 	/* If finished */
-	if (m_FlowStats.getCount() > REQ_FLOW_INIT_COUNT)
+	if (m_FlowQStats.getCount() > REQ_FLOW_INIT_COUNT)
 	{
 		(void) CFE_EVS_SendEvent(PE_FLOW_OK_INF_EID, CFE_EVS_INFORMATION,
 								 "Flow initialized. Mean: (%d), Std dev: (%d)",
@@ -41,7 +39,6 @@ void PE::flowInit()
 int32 PE::flowMeasure(math::Vector2F &y)
 {
 	int32 Status = CFE_SUCCESS;
-	math::Euler euler;
 	float d = 0;
 	float flow_x_rad = 0;
 	float flow_y_rad = 0;
@@ -57,32 +54,37 @@ int32 PE::flowMeasure(math::Vector2F &y)
 		goto flowMeasure_Exit_Tag;
 	}
 
-	// check for agl
+	/* Check if AGL valid */
+	if (!m_Params.ULR_FUSE) {
+		Status = -1;
+		goto flowMeasure_Exit_Tag;
+	}
+
+	/* Check for AGL height */
 	if (m_AglLowPass.m_State < FLOW_MIN_AGL) {
 		Status = -1;
 		goto flowMeasure_Exit_Tag;
 	}
 
-	// check quality
-	if (m_OpticalFlowMsg.Quality < FLOW_MIN_QUALITY) {
-		Status = -1;
-		goto flowMeasure_Exit_Tag;
-	}
-
-	// calculate range to center of image for flow
+	/* Check if terrain estimate valid */
 	if (!m_TzEstValid) {
 		Status = -1;
 		goto flowMeasure_Exit_Tag;
 	}
 
-	//euler = (math::Quaternion(m_VehicleAttitudeMsg.Q)).ToEuler(); TODO
+	/* Check reported quality */
+	if (m_OpticalFlowMsg.Quality < m_Params.FLOW_QUALITY_MIN) {
+		Status = -1;
+		goto flowMeasure_Exit_Tag;
+	}
 
-	d = m_AglLowPass.m_State * cosf(euler[0]) * cosf(euler[1]);
+	/* Calculate range to center of image for flow */
+	d = m_AglLowPass.m_State * cosf(m_Euler[0]) * cosf(m_Euler[1]);
 
-	// optical flow in x, y axis
-	// TODO consider making flow scale a state of the kalman filter
-	flow_x_rad = m_OpticalFlowMsg.PixelFlowXIntegral;// TODO * _flow_scale.get();
-	flow_y_rad = m_OpticalFlowMsg.PixelFlowYIntegral;//TODO * _flow_scale.get();
+	/* Optical flow in x, y axis */
+	/* Note: Consider making flow scale a state of the kalman filter */
+	flow_x_rad = m_OpticalFlowMsg.PixelFlowXIntegral * m_Params.FLOW_SCALE;
+	flow_y_rad = m_OpticalFlowMsg.PixelFlowYIntegral * m_Params.FLOW_SCALE;
 	dt_flow = m_OpticalFlowMsg.IntegrationTimespan / 1.0e6f;
 
 	if (dt_flow > 0.5f || dt_flow < 1.0e-6f) {
@@ -90,7 +92,7 @@ int32 PE::flowMeasure(math::Vector2F &y)
 		goto flowMeasure_Exit_Tag;
 	}
 
-	// angular rotation in x, y axis
+	/* Angular rotation in x, y axis */
 	gyro_x_rad = 0;
 	gyro_y_rad = 0;
 
@@ -101,22 +103,20 @@ int32 PE::flowMeasure(math::Vector2F &y)
 //					 _sub_flow.get().gyro_y_rate_integral);
 //	}
 
-	// compute velocities in body frame using ground distance
-	// note that the integral rates in the optical_flow uORB topic are RH rotations about body axes
+	/* Compute velocities in body frame using ground distance */
+	/* Note: Integral rates in the optical_flow msg are RH rotations about body axes */
 	delta_b[0] = float(fabs(flow_y_rad - gyro_y_rad) * d);
 	delta_b[1] = -(flow_x_rad - gyro_x_rad) * d;
 	delta_b[2] = 0.0f;
 
-	// rotation of flow from body to nav frame
+	/* Rotation of flow from body to nav frame */
 	delta_n = m_RotationMat * delta_b;
 
-	// imporant to timestamp flow even if distance is bad
-	m_TimeLastFlow = m_Timestamp;
-
-	// measurement
+	/* Measurement */
 	y[Y_flow_vx] = delta_n[0] / dt_flow;
 	y[Y_flow_vy] = delta_n[1] / dt_flow;
 
+	m_TimeLastFlow = m_Timestamp;
 	//m_FlowQStats.update(Scalarf(m_OpticalFlowMsg.Quality));
 	m_FlowQStats.update(float(m_OpticalFlowMsg.Quality));
 
@@ -132,26 +132,26 @@ void PE::flowCorrect()
 	const float h_max = 8.0f;
 	const float v_min = 0.5f;
 	const float v_max = 1.0f;
-	const float p[5] = {0.04005232f, -0.00656446f, -0.26265873f,  0.13686658f, -0.00397357f};
     float h = 0;
     float v = 0;
     float flow_vxy_stddev = 0;
     float rotrate_sq = 0;
     float rot_sq = 0;
 
+    /* Polynomial noise model, found using least squares fit
+	** h, h**2, v, v*h, v*h**2  */
+	const float p[5] = {0.04005232f, -0.00656446f, -0.26265873f,  0.13686658f, -0.00397357f};
+
     if (flowMeasure(m_Flow.y) != CFE_SUCCESS)
     {
         goto end_of_function;
     }
 
-    /* measured altitude, negative down dir */
+    /* Measured altitude, negative down dir */
     m_Flow.C[Y_flow_vx][X_vx] = 1.0f;
     m_Flow.C[Y_flow_vy][X_vy] = 1.0f;
 
-    // polynomial noise model, found using least squares fit
-	// h, h**2, v, v*h, v*h**2
-
-	// prevent extrapolation past end of polynomial fit by bounding independent variables
+	/* Prevent extrapolation past end of polynomial fit by bounding independent variables */
 	h = m_AglLowPass.m_State;
 	v = sqrtf(m_Flow.y * m_Flow.y); //was norm()
 
@@ -171,7 +171,7 @@ void PE::flowCorrect()
 		v = v_min;
 	}
 
-	// compute polynomial value
+	/* Compute polynomial value */
 	flow_vxy_stddev = p[0] * h + p[1] * h * h + p[2] * v + p[3] * v * h + p[4] * v * h * h;
 
 	rotrate_sq = m_VehicleAttitudeMsg.RollSpeed * m_VehicleAttitudeMsg.RollSpeed
@@ -180,15 +180,20 @@ void PE::flowCorrect()
 
 	rot_sq = m_Euler[0] * m_Euler[0] + m_Euler[1] * m_Euler[1];
 
+	m_Flow.R[Y_flow_vx][Y_flow_vx] = flow_vxy_stddev * flow_vxy_stddev +
+			m_Params.FLOW_R * m_Params.FLOW_R * rot_sq +
+			m_Params.FLOW_RR * m_Params.FLOW_RR * rotrate_sq;
+	m_Flow.R[Y_flow_vy][Y_flow_vy] = m_Flow.R[Y_flow_vx][Y_flow_vx];
+
     /* Vector2F -  (2x10 * Vector10F) */
     m_Flow.r = m_Flow.y - (m_Flow.C * m_StateVec);
 
-    /* residual */
+    /* Residual */
     /* ((2x10 * 10x10) * 10x2) + 2x2) */
     m_Flow.S_I = m_Flow.C * m_StateCov * m_Flow.C.Transpose() + m_Flow.R;
     m_Flow.S_I = m_Flow.S_I.Inversed();
 
-    /* fault detection 1F * 1x1 * 1F */
+    /* Fault detection 1x2 * 2x2 * 2F */
     m_Flow.beta = (m_Flow.r.Transpose() * (m_Flow.S_I * m_Flow.r)); //todo was [0][0]
 
     if (m_Flow.beta > FLOW_BETA_MAX)
@@ -213,7 +218,7 @@ void PE::flowCorrect()
         m_FlowInitialized = TRUE;
     }
 
-    /* kalman filter correction */
+    /* Kalman filter correction */
     if (!m_FlowFault)
     {
 		/* 10x10 * 10x2 * 2x2 */
@@ -252,7 +257,7 @@ void PE::flowCheckTimeout()
 		if (!m_FlowTimeout)
 		{
 			m_FlowTimeout = TRUE;
-			m_FlowStats.reset();
+			m_FlowQStats.reset();
 			(void) CFE_EVS_SendEvent(PE_FLOW_TIMEOUT_ERR_EID, CFE_EVS_ERROR,
 									 "Flow timeout: %llu us", Timestamp);
 		}
